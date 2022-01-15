4837b8c46678bf4749639d26a009bce5b3830f74
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-01-14 19:01:09 -0500

    Installed cookie parser, connect mongo, express session Exported mongoose connection from DB Set up middleware for server to use sessions with mongo store Set up middleware for CORS Set up middleware for a session shopping cart

--- server/config/database.js
+++ server/config/database.js
@@ -5,6 +5,7 @@ export const connectToDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connection SUCCESS");
+    return mongoose.connection;
   } catch (error) {
     console.error(`Error while connecting to database: ${error}`);
     process.exit(1);
--- server/server.js
+++ server/server.js
@@ -3,16 +3,41 @@ import express from "express";
 import cors from "cors";
 import { connectToDatabase } from "./config/database";
 import productRouter from "./routes/product.router";
+import cartRouter from "./routes/cart.router";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import cookieParser from "cookie-parser";
 
-connectToDatabase();
+const dbConnection = connectToDatabase();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://192.168.2.239:3000", credentials: true }));
+app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      client: dbConnection.then((data) => data.getClient()),
+    }),
+    cookie: { maxAge: 1000 * 60 * 60 },
+  })
+);
 
+//session can be set in 'locals' of response of any middleware
+app.use((req, res, next) => {
+  if (!req.session.cart) {
+    req.session.cart = [];
+    req.session.save;
+  }
+  next();
+});
 app.use("/api/products", productRouter);
+app.use("/api/cart", cartRouter);
 
 app.use((req, res, next) => {
   res.status(404);
