fd65494c8e54c0d0a2574c6c607147e0edaa9ac9
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-01-17 18:30:31 -0500

    Set up middleware for passport and csurf. Fixed cors middelware to use environment variables

--- server/server.js
+++ server/server.js
@@ -5,15 +5,20 @@ import { connectToDatabase } from "./config/database";
 import productRouter from "./routes/product.router";
 import cartRouter from "./routes/cart.router";
 import stripeRouter from "./routes/stripe.router";
+import userRouter from "./routes/user.router";
 import session from "express-session";
 import MongoStore from "connect-mongo";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import csurf from "csurf";
 
 const dbConnection = connectToDatabase();
+const csrfProtection = csurf();
 
 const app = express();
+require("./config/passport");
 
-app.use(cors({ origin: "http://192.168.2.239:3000", credentials: true }));
+app.use(cors({ origin: `${process.env.HOSTNAME}:3000`, credentials: true }));
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +33,16 @@ app.use(
     cookie: { maxAge: 1000 * 60 * 60 },
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+  if (!req.session.token) {
+    req.session.token = req.csrfToken();
+  }
+  next();
+});
 
 //session can be set in 'locals' of response of any middleware
 app.use("/api/cart", (req, res, next) => {
@@ -39,6 +54,7 @@ app.use("/api/cart", (req, res, next) => {
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
 app.use(stripeRouter);
+app.use("/user", userRouter);
 
 app.use((req, res, next) => {
   res.status(404);
